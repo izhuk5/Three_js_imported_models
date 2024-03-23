c0a31877639aa9fb2a68001eeed4b84ebd99e416
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-03-23 22:56:10 +0200

    animations

--- src/script.js
+++ src/script.js
@@ -24,11 +24,17 @@ dracoLoader.setDecoderPath("/draco/");
 const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 
-gltfLoader.load("/models/Duck/glTF-Draco/Duck.gltf", gltf => {
-  // const children = [...gltf.scene.children];
-  // for (const child of children) {
-  //   scene.add(child);
-  // }
+let mixer = null;
+
+gltfLoader.load("/models/Fox/glTF/Fox.gltf", gltf => {
+  console.log(gltf);
+
+  mixer = new THREE.AnimationMixer(gltf.scene);
+  const action = mixer.clipAction(gltf.animations[2]);
+
+  action.play();
+
+  gltf.scene.scale.set(0.025, 0.025, 0.025);
   scene.add(gltf.scene);
 });
 
@@ -125,6 +131,11 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - previousTime;
   previousTime = elapsedTime;
+
+  // Update mixer
+  if (mixer !== null) {
+    mixer.update(deltaTime);
+  }
 
   // Update controls
   controls.update();
